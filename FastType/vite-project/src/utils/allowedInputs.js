b71abd326c6ef0eb09689aa3a92734299fd0fd5b
Jason <55802733+JS0Nwong@users.noreply.github.com>
2024-01-09 01:08:22 -0500

    Delete FastType/vite-project directory

--- FastType/vite-project/src/utils/allowedInputs.js
+++
@@ -1,8 +0,0 @@
-const allowedInputs = (code) => {
-    return (
-        code.startsWith('Key') ||
-        code === 'Backspace' ||
-        code === 'Space'
-    )
-}
-export { allowedInputs };
--- FastType/vite-project/src/utils/generateWords.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { generate, count } from "random-words";
-
-export default function generateWords() {
-  const words = generate({exactly: 40})
-  return words;
-}
