84f1ee2f2417e31510413e87008d7fbd966c3689
Jason <55802733+JS0Nwong@users.noreply.github.com>
2024-01-09 01:08:30 -0500

    Delete New folder (3)/todo directory

--- New folder (3)/todo/src/utils/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "./AuthProvider";
-import useStore from "../store";
-
-export default function PrivateRoute({ Component }) {
-    const { isLoggedIn } = useStore();
-    const { currentUser, isLoading } = useAuth()
-    return currentUser ? <Component /> : <Navigate to="/" replace/>
-}   
--- New folder (3)/todo/src/utils/PublicRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "./AuthProvider";
-import useStore from "../store";
-
-export default function PublicRoute({ Component }) {
-    const { currentUser } = useAuth()
-    return !currentUser ? <Component /> : <Navigate to="/boards" replace />;
-}
